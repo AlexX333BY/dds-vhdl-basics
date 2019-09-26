86631e5fcf2c46e0c10dc445162e69a9bb0c3a60
Aliaksei <alexx333by@protonmail.com>
2019-09-26 23:30:15 +0300

    Renamed Q to QElement

--- Q/QElement.vhd
+++ Q/QElement.vhd
@@ -1,14 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Q is
+entity QElement is
     Port ( in1 : in  STD_LOGIC;
            in2 : in  STD_LOGIC;
            in3 : in  STD_LOGIC;
            Q : out  STD_LOGIC);
-end Q;
+end QElement;
 
-architecture Behavioral of Q is
+architecture Behavioral of QElement is
    signal in12, in23: std_logic;
 begin
    in12 <= in1 AND in2;
